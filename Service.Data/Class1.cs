c64399d4a45594967cdc6aa97b9c10efa65c30be
Mark Jones <mark@Marks-MacBook-Air.local>
2016-01-09 16:16:04 +0000

    Initial project creation

---
+++ Service.Data/Class1.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Service.Data
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+    }
+}
